a170590d5b37795976602c510946fc1f9ddc1371
Nicolas Chatelain <n.chatelain@cgi.com>
2025-11-18 20:40:58 +0100

    add file header

--- pkg/utils/ligolourl.go
+++ pkg/utils/ligolourl.go
@@ -1,3 +1,19 @@
+// Ligolo-ng
+// Copyright (C) 2025 Nicolas Chatelain (nicocha30)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package utils
 
 import (
@@ -45,7 +61,6 @@ func ParseLigoloURL(rawURL string) (*LigoloURL, error) {
 	return &LigoloURL{u}, nil
 }
 
-// helper pour afficher joliment les parties de l'URL
 func printURL(title string, u *url.URL) {
 	fmt.Printf("--- %s ---\n", title)
 	if u == nil {
